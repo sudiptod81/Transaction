5af9de5469453053928eff84784d39e29ff79b65
Sudipto Das <sudiptod81@hotmail.com>
2015-12-28 13:28:49 +0530

    Remember tokenfield added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,16 @@
 class SessionsController < ApplicationController
   def new
+    @user=User.find(1)
+    #@user=User.new(:username=>'bleh',:password=>'bleh')
   end
   def create
+    user = User.find_by_username(params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to search
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+    end
   end
   def destroy
   end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -6,7 +6,19 @@ describe "Authentication" do
     describe "with invalid information" do
       before { click button "Sign in" }
       it { should have_selector('title', text: 'Sign in') }
-      it { should have_selector(div.alert.alert-error', text: 'Invalid') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    end
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill in "Email", with: user.email
+        fill in "Password", with: user.password
+        click button "Sign in"
+      end
+      it { should have selector('title', text: user.name) }
+      it { should have link('Profile', href: user path(user)) }
+      it { should have link('Sign out', href: signout path) }
+      it { should not have link('Sign in', href: signin path) }
     end
   end
 end
